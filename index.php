10bed26d4b70c0733fce0f200196be874f3b5504
Kenny Hung <kennysubs+github@outlook.com>
2014-08-12 22:27:32 +0100

    Revert "Initial commit"
    This reverts commit 5282ff110231039c9a4ab7cab5153bf80b2b545b.

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <head>
-    <title>Cacahuetes - Jazz for all occasions</title>
-  </head>
-  <body>
-    <h1>Please hold the line!</h1>
-    <p>We'll be back before you know it :)</p>
-  </body>
-</html>
