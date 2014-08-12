f24f30308d75619aaaf5d72a3e63b56795b9a62e
Kenny Hung <kennysubs+github@outlook.com>
2014-08-12 22:35:37 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,1 @@
+<html>  <head>    <title>Cacahuetes - Jazz for all occasions</title>  </head>  <body>    <h1>Please hold the line!</h1>    <p>We'll be back before you know it :)</p>  </body></html>
