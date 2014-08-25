6f5eface68f3f3e337caa59c82450d71bb81db15
Kenny Hung <kennysubs+github@outlook.com>
2014-08-25 15:18:27 +0100

    File insensitive image paths

--- Framework/FilePathHelper.php
+++ Framework/FilePathHelper.php
@@ -1,13 +1,13 @@
 <?php
 
 class FilePathHelper {
-  
+
   private $slash;
-  
+
   public function __construct() {
     $this->slash = DIRECTORY_SEPARATOR;
   }
-  
+
   public function JoinPaths() {
     $arguments = func_get_args();
     if (count($arguments) === 1 && is_array($arguments[0])) {
@@ -16,11 +16,31 @@ class FilePathHelper {
       return $this->JoinPathsArray($arguments);
     }
   }
-  
-  private function JoinPathsArray($arguments){
-    
+
+  private function JoinPathsArray($arguments) {
     $sections = preg_split(
             "@[/\\\\]@", implode('/', $arguments), null, PREG_SPLIT_NO_EMPTY);
     return implode($this->slash, $sections);
   }
+
+  public function FileExists($fileName, $caseSensitive = false) {
+
+    if (file_exists($fileName)) {
+      return $fileName;
+    }
+    if ($caseSensitive) {
+      return false;
+    }
+
+    $directoryName = dirname($fileName);
+    $fileArray = glob($directoryName . '/*', GLOB_NOSORT);
+    $fileNameLowerCase = strtolower($fileName);
+    foreach ($fileArray as $file) {
+      if (strtolower($file) == $fileNameLowerCase) {
+        return $file;
+      }
+    }
+    return false;
+  }
+
 }
--- Framework/ImageDetails.php
+++ Framework/ImageDetails.php
@@ -1,23 +1,22 @@
 <?php
 
 class ImageDetails {
+
   private $filePathHelper;
-  
   private $pathToImages;
-  
   private $imageName;
-  
   public $path;
-  
   public $contentTypeHeader;
-  
+
   // So far, this only supports jpg files
   // TODO: Support other types
   public function __construct($pathToImages, $imageName) {
     $this->filePathHelper = new FilePathHelper();
     $this->pathToImages = $pathToImages;
     $this->imageName = $imageName;
-    $this->path = $this->filePathHelper->JoinPaths($pathToImages, $imageName . '.jpg');
+    $path = $this->filePathHelper->JoinPaths($pathToImages, $imageName . '.jpg');
+    $this->path = $this->filePathHelper->FileExists($path);
     $this->contentTypeHeader = 'Content-Type:image/jpeg';
   }
+
 }
